48c59c8cbb1fd14e2c175679b615e3a1277bb42c
dina252006 <161189192+dina252006@users.noreply.github.com>
2024-10-20 15:04:46 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+
+//routes
+app.use(require('./routes/router'))
+app.use(require('./routes/todo'))
+
+
+
+mongoose.connect("mongodb+srv://kozhaevadina15sh:L2oMR8KKKwPydTcO@backenddb.z7dj3.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+    .then(() => {
+        console.log("Connected to database!");
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch((error) => {
+        console.log("Connection failed!", error);
+    });
+
+
+
+
---
+++ routes/todo.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+
+router.post("/add/todo", (req, res) => {
+    const {todo} = req.body;
+    console.log(todo);
+})
+
+module.exports = router;

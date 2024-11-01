8f081e1efc84f6d58a89f023bf99ce5cda3ebdd4
dina252006 <161189192+dina252006@users.noreply.github.com>
2024-11-01 14:07:25 +0500

    index.js

--- index.js
+++ index.js
@@ -11,13 +11,12 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
-//routes
 app.use(require('./routes/router'))
 app.use(require('./routes/todo'))
 
 
 
-mongoose.connect("mongodb+srv://kozhaevadina15sh:L2oMR8KKKwPydTcO@backenddb.z7dj3.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+mongoose.connect("mongodb+srv://kozhaevadina15sh:lB1wLkADtpB1NxX5@backenddb.z7dj3.mongodb.net/BackendDB?retryWrites=true&w=majority")
     .then(() => {
         console.log("Connected to database!");
         app.listen(3000, () => {
